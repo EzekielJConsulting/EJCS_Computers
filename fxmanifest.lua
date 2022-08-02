50da383e4ffd8af7bf46a25fe733f08c67b38eb5
Jafesu <Jafesu@users.noreply.github.com>
2022-08-02 01:09:17 -0500

    EJCSCOMP-5 - Add Base FXManifest"

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'EzekielJ Development Studios'
+description 'Immersive Complex in-game computer system'
+version '0.0.1'
+
+shared_scripts {
+
+}
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'config.lua'
+}
+client_scripts {
+    'config.lua',
+}
+files {
+    
+}
+
+
+dependencies {
+    'es_extended',
+    'qtarget',
+    
+}
